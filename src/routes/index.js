cdff49dd062085839b1b5876abbc56c2f73301a2
Carlos-Monteiro26 <dev.carlos.monteiro@gmail.com>
2024-03-04 11:12:53 -0300

    feat: forgot and reset password implemented

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,8 @@ const routes = new Router();
 //-------- Unauthenticated routes ----------
 routes.post("/user", UserController.create);
 routes.post("/login", UserController.login);
+routes.post("/forgot-password", UserController.forgotPassword);
+routes.post("/reset-password", UserController.resetPassword);
 
 //-------- Authenticated routes ----------
 routes.use(authMiddleware);
